698e007635680134246c7cd063af3797b207b4b1
Rubén Soler <r.solerginer@gmail.com>
2025-10-18 08:14:08 +0200

    Add comments

---
+++ 01-hola-mundo/comentarios.py
@@ -0,0 +1,5 @@
+# Esto es un comentario en Python
+print("Hola, mundo!")  # Esto imprime un mensaje en la consola
+"""Este es un comentario
+de varias líneas
+que explica el propósito del código."""
