d9d4d5aa6b49eb17200f3c9588fd5af7beb47344
Rubén Soler <r.solerginer@gmail.com>
2025-10-18 09:14:47 +0200

    Add types

---
+++ 02-variables-y-tipos/tipos.py
@@ -0,0 +1,32 @@
+nada = None
+booleano = True  # o False
+entero = 42
+flotante = 3.14
+cadena = "Hola, Python"
+lista = [1, 2, 3, 4, 5]
+tuplaNoCambia = (10, 20, 30)
+diccionario = {
+  "nombre": "Rubén",
+  "apellido": "Soler",
+  "edad": 30
+}
+rango = range(1, 10)
+
+dato_byte = b"Esto es un dato en bytes"
+
+print(diccionario)
+print(nada)
+print(lista)
+print(tuplaNoCambia)
+print(dato_byte)  
+print(rango)
+print(type(nada))  # <class 'NoneType'>
+print(type(booleano))  # <class 'bool'>
+print(type(entero))  # <class 'int'>
+print(type(flotante))  # <class 'float'>
+print(type(cadena))  # <class 'str'>
+print(type(lista))  # <class 'list'>
+print(type(tuplaNoCambia))  # <class 'tuple'>
+print(type(diccionario))  # <class 'dict'>
+print(type(rango))  # <class 'range'>
+print(type(dato_byte))  # <class 'bytes'>
---
+++ 02-variables-y-tipos/variables.py
@@ -0,0 +1,12 @@
+texto = "Mastering Python"
+nombre = "Rubén"
+apellido = "Soler"
+
+print(texto)
+
+# Concatenación de cadenas
+
+print(nombre + " " + apellido)
+print(f"{nombre} {apellido}")
+print("Hola me llamo {} {}".format(nombre, apellido))
+print(nombre, apellido)
