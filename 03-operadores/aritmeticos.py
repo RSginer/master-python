dab4d65440fd7bc150473d711fbc616114307e24
Rubén Soler <r.solerginer@gmail.com>
2025-10-21 07:15:43 +0200

    Add opeerators

---
+++ 03-operadores/aritmeticos.py
@@ -0,0 +1,8 @@
+"""Operadores aritméticos en Python."""
+a = 10
+b = 5
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+modulo = a % b
---
+++ 03-operadores/asignacion.py
@@ -0,0 +1,6 @@
+"""Operadores de asignación en Python."""
+a = 10
+b = 5
+a += b
+b *= 2
+
