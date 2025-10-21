d813f98de068f272bee1971ec1c81f0ca06760bf
Rubén Soler <r.solerginer@gmail.com>
2025-10-21 08:16:25 +0200

    Add conditionals

---
+++ 05-condicionales/main.py
@@ -0,0 +1,19 @@
+
+color = input("Ingresa un color: ")
+
+if color == "rojo":
+    print("El color es rojo")
+elif color == "azul":
+    print("El color es azul")
+else:
+    print("El color no es ni rojo ni azul")
+
+"""Operadores de comparación en Python."""
+a = 10
+b = 5
+print(a == b)  # Igualdad
+print(a != b)  # Desigualdad
+print(a > b)   # Mayor que
+print(a < b)   # Menor que
+print(a >= b)  # Mayor o igual que
+print(a <= b)  # Menor o igual que
