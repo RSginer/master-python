f7219a81b635d98896e87f0ad00c69374eb83881
Rubén Soler <r.solerginer@gmail.com>
2025-10-28 07:16:17 +0100

    Add bucles

---
+++ 06-bucles/bucle-for.py
@@ -0,0 +1,3 @@
+
+for contador in range(1, 7):
+    print("El contador es:", contador)
---
+++ 06-bucles/bucle-while.py
@@ -0,0 +1,5 @@
+contador = 0
+
+while contador < 100:
+    print("El contador es:", contador)
+    contador += 1
