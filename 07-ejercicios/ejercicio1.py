59916aa5c696e862a253279e58cf974fdc26427c
Rubén Soler <r.solerginer@gmail.com>
2025-10-28 07:26:43 +0100

    Add ejercicio 1

---
+++ 07-ejercicios/ejercicio1.py
@@ -0,0 +1,13 @@
+"""
+Ejercicio 1: 
+
+- Crear dos variables una pais y otra continente.
+- Mostrar su valor por pantalla (imprimir).
+- Poner un comentario diciendo el tipo de dato de cada variable.
+
+"""
+pais = "España"          # Tipo de dato: str (cadena de texto)
+continente = "Europa"    # Tipo de dato: str (cadena de texto)
+
+print("País:", pais)
+print("Continente:", continente)
