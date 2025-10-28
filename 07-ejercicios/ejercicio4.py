e93eeb317d413ce5de394a0b6e2da9baecef5868
Rubén Soler <r.solerginer@gmail.com>
2025-10-28 08:37:13 +0100

    Add ejercicio4

---
+++ 07-ejercicios/ejercicio4.py
@@ -0,0 +1,23 @@
+"""
+Ejercicio 4.
+Pedir dos numeros al usuario y hacer todas las operaciones básicas de una calculadora
+"""
+
+import math
+
+
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2 if numero2 != 0 else "Error: División por cero no permitida"
+redondeo = math.ceil(numero1)
+print(f"Resultados:")
+print(f"Suma: {suma}")
+print(f"Resta: {resta}")
+print(f"Multiplicación: {multiplicacion}")
+print(f"División: {division}")
+print(f"Redondeo: {redondeo}")
+print("---END CALCULADORA---")
