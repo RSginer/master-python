e08748dd534323c47fc8c0b97f174d85fdac0840
Rubén Soler <r.solerginer@gmail.com>
2025-10-28 08:34:36 +0100

    Add ejercicio3

---
+++ 07-ejercicios/ejercicio3.py
@@ -0,0 +1,14 @@
+"""
+Ejercicio 3.
+Escribe un programa que muestre los cuadrados de los 60 primeros números naturales.
+"""
+for i in range(1, 61):
+    print(i**2)
+
+print("---END FOR---")
+i = 1
+while i < 61:
+    print(i**2)
+    i += 1
+
+print("---END WHILE---")
