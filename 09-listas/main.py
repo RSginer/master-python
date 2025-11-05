6df2619a570eea72bd8b9c2364b67dca5b5dcb8b
Rubén Soler <r.solerginer@gmail.com>
2025-11-05 08:31:11 +0100

    add lists

--- 09-listas/main.py
+++ 09-listas/main.py
@@ -10,3 +10,14 @@ print(type(peliculas))
 print(type(canciones))
 print(peliculas[0:2])
 print(canciones[1:])
+
+canciones.append("Thriller")
+print(canciones)
+
+## Recorrer listas
+
+for pelicula in peliculas:
+  print(f"{peliculas.index(pelicula) + 1}. Película: {pelicula}")
+
+for cancion in canciones:
+  print(f"{canciones.index(cancion) + 1}. Canción: {cancion}")
