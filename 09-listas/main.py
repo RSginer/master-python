ee7317e15404e40815b007a7df322808c1f0222c
Rubén Soler <r.solerginer@gmail.com>
2025-11-05 08:10:38 +0100

    add listas

---
+++ 09-listas/main.py
@@ -0,0 +1,12 @@
+"""
+Listas
+"""
+peliculas = ["Batman", "Superman", "Wonder Woman"]
+canciones = list(("Imagine", "One", "Billie Jean"))
+print(peliculas)
+
+peliculas.remove("Superman")
+print(type(peliculas))
+print(type(canciones))
+print(peliculas[0:2])
+print(canciones[1:])
