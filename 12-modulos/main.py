f399b866f55fa544aab7ff2602e01130b80ff01e
Rubén Soler <r.solerginer@gmail.com>
2025-11-07 07:44:32 +0100

    ADd modules

---
+++ 12-modulos/main.py
@@ -0,0 +1,5 @@
+from mimodulo import suma
+
+resultado = suma(5, 7)
+
+print("El resultado de la suma es:", resultado)
---
+++ 12-modulos/mimodulo.py
@@ -0,0 +1,8 @@
+"""
+Modulos
+"""
+def hola_mundo():
+    print("Hola Mundo desde un módulo")
+
+def suma(a, b):
+    return a + b
