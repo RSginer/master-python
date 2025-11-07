7f5d39cb7b2c218269cd223bb29c3528efb0d683
Rubén Soler <r.solerginer@gmail.com>
2025-11-07 08:07:57 +0100

    add use modules

--- 12-modulos/main.py
+++ 12-modulos/main.py
@@ -1,5 +1,12 @@
 from mimodulo import suma
+from datetime import datetime
+from math import sqrt, pi
 
 resultado = suma(5, 7)
 
 print("El resultado de la suma es:", resultado)
+
+print(datetime.now().strftime("%d/%m/%Y"))
+
+print(sqrt(16))
+print("El valor de pi es:", pi)
