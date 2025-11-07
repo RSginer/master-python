721d230696b339d4e68144070e03b1dfcfdebf8a
Rubén Soler <r.solerginer@gmail.com>
2025-11-07 07:22:27 +0100

    add diccionarios

---
+++ 10-sets-diccionarios/diccionarios.py
@@ -0,0 +1,26 @@
+"""
+Diccionario:
+Un tipo de dato que almacena pares clave-valor.
+"""
+
+persona = {
+  "nombre": "Juan",
+  "edad": 30,
+  "ciudad": "Madrid"
+}
+
+print(persona["ciudad"])
+print(type(persona['nombre']))
+
+# lista de diccionarios
+
+contactos = [
+  {
+    "nombre": "Ana",
+    "telefono": "123456789"
+  },
+  {
+    "nombre": "Luis",
+    "telefono": "987654321"
+  }
+]
