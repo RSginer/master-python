212bb0d0eb6f724a7626477cec893b3945f073f4
Rubén Soler <r.solerginer@gmail.com>
2025-11-07 08:17:29 +0100

    add random

--- 12-modulos/main.py
+++ 12-modulos/main.py
@@ -1,6 +1,7 @@
 from mimodulo import suma
 from datetime import datetime
 from math import sqrt, pi
+from random import randint
 
 resultado = suma(5, 7)
 
@@ -10,3 +11,5 @@ print(datetime.now().strftime("%d/%m/%Y"))
 
 print(sqrt(16))
 print("El valor de pi es:", pi)
+
+print("Número aleatorio entre 1 y 10:", randint(1, 10))
